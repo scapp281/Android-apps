031cebc356ec288826975981dd10cd448903891d
Stephen Chin <scapp281@gmail.com>
2016-07-12 01:05:23 -0400

    use Arraylist instead, code to add textview to Number laylout

--- Android-Mutiscreen/Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Android-Mutiscreen/Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,12 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,16 +16,26 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        String[] words = new String[10];
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
+        ArrayList<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        for (int i = 0; i < words.size(); i++) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(i));
+            rootView.addView(wordView);
+        }
+
     }
 }
