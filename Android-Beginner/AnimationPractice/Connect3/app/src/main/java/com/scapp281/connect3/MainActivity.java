44954098f3b4faffd3e546c025a233b91a8e47af
Stephen Chin <scapp281@gmail.com>
2016-11-07 19:10:15 -0500

    winning states, play again layout

--- Android-Beginner/AnimationPractice/Connect3/app/src/main/java/com/scapp281/connect3/MainActivity.java
+++ Android-Beginner/AnimationPractice/Connect3/app/src/main/java/com/scapp281/connect3/MainActivity.java
@@ -3,12 +3,16 @@ package com.scapp281.connect3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     int activePlayer = 0;
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
 
     public void dropIn(View view) {
         ImageView counter = (ImageView) view;
@@ -26,6 +30,25 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
+
+
+        for (int[] winningPosition : winningPositions) {
+            if(gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
+                    gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
+                    gameState[winningPosition[0]] != 2){
+
+                LinearLayout playAgainLayout = (LinearLayout) findViewById(R.id.playAgainLayout);
+                Animation slide_down = AnimationUtils.loadAnimation(getApplicationContext(),
+                        R.anim.slide_down);
+
+                Animation slide_up = AnimationUtils.loadAnimation(getApplicationContext(),
+                        R.anim.slide_up);
+
+                playAgainLayout.setVisibility(View.VISIBLE);
+                playAgainLayout.startAnimation(slide_down);
+            }
+
+        }
     }
 
     @Override
