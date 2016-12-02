b7932d418bef96cb75cd8883e459437e82c73139
Stephen Chin <scapp281@gmail.com>
2016-12-02 17:10:50 -0500

    game layout

---
+++ Android-Beginner/BrainTeaser/app/src/main/java/com/scapp281/brainteaser/MainActivity.java
@@ -0,0 +1,100 @@
+package com.scapp281.brainteaser;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
+    TextView timerTextview;
+    TextView pointsTextview;
+    TextView sumTextview;
+    TextView resultTextview;
+    GridLayout answersGrid;
+    Button button0, button1, button2, button3;
+    CountDownTimer showMainGameScreen;
+    ArrayList<Integer> answers = new ArrayList<Integer>();
+    int locationOfCorrectAnswer;
+
+    public void chooseAnswer(View view){
+
+    }
+    public void start(View view) {
+        // TODO: Learn to do sets of animation
+        startButton.animate().alpha(1f).rotation(2000f).translationXBy(1800f).setDuration(4000);
+
+        showMainGameScreen = new CountDownTimer(3000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                timerTextview.setVisibility(View.VISIBLE);
+                pointsTextview.setVisibility(View.VISIBLE);
+                sumTextview.setVisibility(View.VISIBLE);
+                answersGrid.setVisibility(View.VISIBLE);
+            }
+        }.start();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startButton = (Button) findViewById(R.id.startButton);
+        startButton.setVisibility(View.VISIBLE);
+        timerTextview = (TextView) findViewById(R.id.timerTextview);
+        pointsTextview = (TextView) findViewById(R.id.pointsTextView);
+        sumTextview = (TextView) findViewById(R.id.sumTextview);
+        resultTextview = (TextView) findViewById(R.id.resultTextview);
+        answersGrid = (GridLayout) findViewById(R.id.answersGrid);
+        button0 = (Button) findViewById(R.id.button0);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        timerTextview.setVisibility(View.GONE);
+        pointsTextview.setVisibility(View.GONE);
+        sumTextview.setVisibility(View.GONE);
+        resultTextview.setVisibility(View.GONE);
+        answersGrid.setVisibility(View.GONE);
+
+        Random random = new Random();
+
+        int a = random.nextInt(50);
+        int b = random.nextInt(50);
+
+        sumTextview.setText(Integer.toString(a) + " + " + Integer.toString(b));
+        locationOfCorrectAnswer = random.nextInt(4);
+
+        for (int i = 0; i < 4; i++) {
+            if (i == locationOfCorrectAnswer) {
+                answers.add(a + b);
+            } else {
+                int incorrectAnswer = random.nextInt(41);
+                while (incorrectAnswer == a + b) {
+                    incorrectAnswer = random.nextInt(41);
+                }
+                answers.add(incorrectAnswer);
+            }
+        }
+
+        button0.setText(Integer.toString(answers.get(0)));
+        button1.setText(Integer.toString(answers.get(1)));
+        button2.setText(Integer.toString(answers.get(2)));
+        button3.setText(Integer.toString(answers.get(3)));
+    }
+}
