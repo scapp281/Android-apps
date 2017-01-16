4777d47dc1e001cf31ce0fceaffbdb803878b149
Stephen Chin <scapp281@gmail.com>
2017-01-15 22:56:34 -0500

    add new note item

--- Android-Advanced/Notebook/app/src/main/java/com/scapp281/notebook/MainActivityListFragment.java
+++ Android-Advanced/Notebook/app/src/main/java/com/scapp281/notebook/MainActivityListFragment.java
@@ -6,11 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivityListFragment extends ListFragment {
 
+    private ArrayList<Note> notes;
+    private NoteAdapter noteAdapter;
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        ArrayList<Note> notes = new ArrayList<Note>();
+        notes.add(new Note("Hello Android","This is a new note body", Note.Category.MAP));
+
+        noteAdapter = new NoteAdapter(getActivity(),notes);
+        setListAdapter(noteAdapter);
     }
 
     @Override
